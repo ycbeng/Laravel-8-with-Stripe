d17b9bc57a6c55627da0b53b18ccf9023028b41a
ycbeng <yangcheebeng@hotmail.com>
2021-11-15 16:26:57 +0800

    add Category UI

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/addCategory', function () {
+    return view('addCategory');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
