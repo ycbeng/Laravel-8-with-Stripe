307e5bb771fce3b14d12925982a964e4566af9fe
ycbeng <yangcheebeng@hotmail.com>
2021-12-17 14:32:45 +0800

    Stripe layout

--- resources/views/myCart.blade.php
+++ resources/views/myCart.blade.php
@@ -58,62 +58,59 @@
 <div class="row">
     <div class="col-sm-1"></div>
     <div class="col-sm-1"></div>
-    <div class="col-sm-10">
-        
-
-    <div class="row">
-    
-        <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-default credit-card-box">
-            <div class="panel-heading" >
-                <div class="row">
-                    <h3>Card Payment</h3>                    
+    <div class="col-sm-10"> 
+        <div class="row">        
+            <div class="col-md-6 col-md-offset-3">
+                <div class="panel panel-default credit-card-box">
+                <div class="panel-heading" >
+                    <div class="row">
+                        <h3>Card Payment</h3>                    
+                    </div>
+                </div>
+                <div class="panel-body">                
+                    <br>                
+                        <div class='form-row row'>
+                            <div class='col-xs-12 col-md-6 form-group required'>
+                            <label class='control-label'>Name on Card</label> 
+                            <input class='form-control' size='4' type='text'>
+                            </div>
+                            <div class='col-xs-12 col-md-6 form-group required'>
+                            <label class='control-label'>Card Number</label> 
+                            <input autocomplete='off' class='form-control card-number' size='20' type='text'>
+                            </div>                           
+                        </div>                        
+                        <div class='form-row row'>
+                            <div class='col-xs-12 col-md-4 form-group cvc required'>
+                            <label class='control-label'>CVC</label> 
+                            <input autocomplete='off' class='form-control card-cvc' placeholder='ex. 311' size='4' type='text'>
+                            </div>
+                            <div class='col-xs-12 col-md-4 form-group expiration required'>
+                            <label class='control-label'>Expiration Month</label> 
+                            <input class='form-control card-expiry-month' placeholder='MM' size='2' type='text'>
+                            </div>
+                            <div class='col-xs-12 col-md-4 form-group expiration required'>
+                            <label class='control-label'>Expiration Year</label> 
+                            <input class='form-control card-expiry-year' placeholder='YYYY' size='4' type='text'>
+                            </div>
+                        </div>
+                    {{-- <div class='form-row row'>
+                        <div class='col-md-12 error form-group hide'>
+                            <div class='alert-danger alert'>Please correct the errors and try
+                                again.
+                            </div>
+                        </div>
+                    </div> --}}
+                        <div class="form-row row">
+                            <div class="col-xs-12">
+                            <button class="btn btn-primary btn-lg btn-block" type="submit">Pay Now</button>
+                            </div>
+                            
+                        </div>
+                    <br><br>
+                </div>
                 </div>
             </div>
-            <div class="panel-body">                
-                <br>                
-                    <div class='form-row row'>
-                        <div class='col-xs-12 col-md-6 form-group required'>
-                        <label class='control-label'>Name on Card</label> 
-                        <input class='form-control' size='4' type='text'>
-                        </div>
-                        <div class='col-xs-12 col-md-6 form-group required'>
-                        <label class='control-label'>Card Number</label> 
-                        <input autocomplete='off' class='form-control card-number' size='20' type='text'>
-                        </div>                           
-                    </div>                        
-                    <div class='form-row row'>
-                        <div class='col-xs-12 col-md-4 form-group cvc required'>
-                        <label class='control-label'>CVC</label> 
-                        <input autocomplete='off' class='form-control card-cvc' placeholder='ex. 311' size='4' type='text'>
-                        </div>
-                        <div class='col-xs-12 col-md-4 form-group expiration required'>
-                        <label class='control-label'>Expiration Month</label> 
-                        <input class='form-control card-expiry-month' placeholder='MM' size='2' type='text'>
-                        </div>
-                        <div class='col-xs-12 col-md-4 form-group expiration required'>
-                        <label class='control-label'>Expiration Year</label> 
-                        <input class='form-control card-expiry-year' placeholder='YYYY' size='4' type='text'>
-                        </div>
-                    </div>
-                {{-- <div class='form-row row'>
-                    <div class='col-md-12 error form-group hide'>
-                        <div class='alert-danger alert'>Please correct the errors and try
-                            again.
-                        </div>
-                    </div>
-                </div> --}}
-                    <div class="form-row row">
-                        <div class="col-xs-12">
-                        <button class="btn btn-primary btn-lg btn-block" type="submit">Pay Now</button>
-                        </div>
-                        <br>
-                    </div>
-                
-            </div>
-            </div>
         </div>
-
 
     <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
     <script type="text/javascript">
