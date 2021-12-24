3d2b2f3d6a70ddcf8930dcc9f560e39191f6a13f
ycbeng <yangcheebeng@hotmail.com>
2021-12-24 13:55:32 +0800

    update order

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,20 +5,45 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Stripe;
+use DB;
+use Auth;
+use Session;
+use App\Models\myCart;
+use App\Models\myOrder;
 
 class PaymentController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
     public function paymentPost(Request $request)
     {
 	       
-	Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
         Stripe\Charge::create ([
-                "amount" => $request->sub*100,
-                "currency" => "MYR",
-                "source" => $request->stripeToken,
-                "description" => "This payment is testing purpose of southern online",
+            "amount" => $request->sub*100,
+            "currency" => "MYR",
+            "source" => $request->stripeToken,
+            "description" => "This payment is testing purpose of southern online",
         ]);
-           
+
+        $newOrder=myOrder::Create([     //create new order in myOrder with the log in userID
+            'paymentStatus'=>'Done',
+            'userID'=>Auth::id(),
+            'amount'=>$request->sub,
+        ]); 
+        
+        $orderID=DB::table('my_orders')->where('userID','=',Auth::id())->orderBy('created_at','desc')->first(); //get the order ID just now created
+        
+        $items=$request->input('cid');
+        foreach($items as $item=>$value){
+            $carts=myCart::find($value); //get the cart item record
+            $carts->orderID=$orderID->id; //binding the orderID with cart item record
+            $carts->save();
+        }
+
+        Session::flash('success','Order succeessully!');   
         return back();
     }
 }
