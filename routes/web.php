3be31be5bc7af09aaff75e5cde3f62f9f12d1dfc
ycbeng <yangcheebeng@hotmail.com>
2021-12-24 13:10:06 +0800

    Create new myOrder model

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::get('/myCart', [App\Http\Controllers\CartController::class, 'showMyCart']
 
 Route::get('/deleteCart/{id}',[App\Http\Controllers\CartController::class,'delete'])->name('delete.cart.item');
 
-Route::post('\checkout', [App\Http\Controllers\PaymentController::class, 'paymentPost'])->name('payment.post');
+Route::post('/checkout', [App\Http\Controllers\PaymentController::class, 'paymentPost'])->name('payment.post');
 
 Auth::routes();
 
